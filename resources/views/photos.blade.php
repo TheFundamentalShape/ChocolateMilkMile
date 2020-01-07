a2738c6786c2e5cfd20053cbc7b329948232e3ae
Luigi Battaglioli <him@theluigi.com>
2020-01-06 22:55:01 -0500

    kinda fixed photos

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -26,25 +26,25 @@
 
 <h1 class="text-center py-2 text-5xl md:text-6xl text-black font-brand">All photos taken by <a href ="https://www.josephkhale.com/">Joseph Hale.</a></h1>
 
-<div class = "flex flex-wrap px-5 ">
-    <div class = "mb-4 px-2 w-1/2  ">
+<div class = "md:flex md:flex-wrap px-5">
+    <div class = "mb-4 px-2 w-full md:w-1/3">
         <img class = "block h-auto w-full" src='/img/mikhail.jpg'>
     </div>
-    <div class = "mb-4 px-2 w-1/2 ">
+    <div class = "mb-4 px-2 w-full md:w-1/3">
         <img class = "block h-auto w-full" src='/img/colinbrother.jpg'>
     </div>
-    <div class = "mb-4 px-2 w-1/2 ">
+    <div class = "mb-4 px-2 w-full md:w-1/3">
         <img class = "block h-auto w-full" src='/img/joequinn.jpg'>
     </div>
 </div>
 <div class = "flex flex-wrap px-5 ">
-    <div class = "mb-4 px-2 w-1/2  ">
+    <div class = "mb-4 px-2 w-full md:w-1/3 ">
         <img class = "block h-auto w-full" src='/img/dante1.jpg'>
     </div>
-    <div class = "mb-4 px-2 w-1/2 ">
+    <div class = "mb-4 px-2 w-full md:w-1/3">
         <img class = "block h-auto w-full" src='/img/sean.jpg'>
     </div>
-    <div class = "mb-4 px-2 w-1/2 ">
+    <div class = "mb-4 px-2 w-full md:w-1/3">
         <img class = "block h-auto w-full" src='/img/dante2.jpg'>
     </div>
 </div>
