99c3924d96d2cb6d3ca7f8b7aa4c4ed066562fb4
Luigi Battaglioli <him@theluigi.com>
2020-01-07 18:52:40 -0500

    moved layouts to template system for consistency reasons

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,93 +1,71 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact us!</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="https://use.typekit.net/suf2eff.css">
-</head>
+@extends('layouts.landing')
 
-<body>
+@section('title')
+    Contact Us!
+@endsection
 
-<div id="navbar" class="bg-chocolate bg-no-repeat bg-cover shadow" style="background-image: url('/img/ripple.png')">
-    <h1 class="text-center py-4 text-4xl md:text-5xl lg:text-6xl text-white font-brand">The Chocolate Milk Mile</h1>
-    <div class="flex justify-center">
-        <a href="/" class="inline-block text-2xl md:text-3xl lg:text-4xl nav-link hover:underline hover:text-active-yellow">Home</a>
-        <a href="/about" class="inline-block text-2xl md:text-3xl lg:text-4xl nav-link hover:underline hover:text-active-yellow">About</a>
-        <a href="/contact" class="inline-block text-2xl md:text-3xl lg:text-4xl nav-link hover:underline hover:text-active-yellow">Contact</a>
-        <a href="/photos" class="inline-block text-2xl md:text-3xl lg:text-4xl nav-link hover:underline hover:text-active-yellow">Photos</a>
-        <a href="/register" class="inline-block text-2xl md:text-3xl lg:text-4xl nav-link hover:underline hover:text-active-yellow">Register</a>
-    </div>
-</div>
+@section('content')
+    <div id="head" class="relative mt-16">
 
-<div id="head" class="relative mt-16">
-
-    <div class="p-4 md:flex">
-        <div class="text-center md:max-w-lg lg:mx-auto">
-            <h1 class="text-chocolate font-brand text-5xl md:text-6xl">Have a question?</h1>
-            <p class="text-black text-3xl md:text-4xl lg:text-5xl font-body">Feel Free to reachout! We’ll get back to you as soon as we possibly can.</p>
-            <p class="text-gray-600 text-2xl md:text-3xl font-body">(You can use the form below to reach us!)</p>
-        </div>
-        <div class="hidden md:block md:absolute md:right-0 md:overflow-hidden">
-            <div class="-mr-40">
-                <img src="/img/cow.png" alt="">
+        <div class="p-4 md:flex">
+            <div class="text-center md:max-w-lg lg:mx-auto">
+                <h1 class="text-chocolate font-brand text-5xl md:text-6xl">Have a question?</h1>
+                <p class="text-black text-3xl md:text-4xl lg:text-5xl font-body">Feel Free to reachout! We’ll get back to you as soon as we possibly can.</p>
+                <p class="text-gray-600 text-2xl md:text-3xl font-body">(You can use the form below to reach us!)</p>
+            </div>
+            <div class="hidden md:block md:absolute md:right-0 md:overflow-hidden">
+                <div class="-mr-40">
+                    <img src="/img/cow.png" alt="">
+                </div>
             </div>
         </div>
+
+        <div class="flex justify-center">
+            <form class="max-w-sm md:max-w-4xl" action="/contact" method="post">
+
+                @csrf
+
+                @if ($errors->any())
+                    <div class="bg-active-yellow rounded shadow-2xl p-4 mt-4">
+                        <h1 class="font-brand text-2xl">Hmm... Something went wrong.</h1>
+                        <ul class="list-disc pl-5">
+                            @foreach ($errors->all() as $error)
+                                <li class="font-body font-xl">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <div class="my-8">
+                    <div>
+                        <label class="font-body text-3xl pr-12" for="">What is your name?</label>
+                    </div>
+                    <input class="border rounded p-2 w-full" type="text" name="name">
+                </div>
+
+                <div class="my-8">
+                    <div>
+                        <label class="font-body text-3xl pr-12" for="">What is your email?</label>
+                    </div>
+                    <input class="border rounded p-2 w-full" type="email" name="email">
+                </div>
+
+                <div class="my-8">
+                    <div>
+                        <label class="font-body text-3xl pr-12" for="">What are ya inquiring about?</label>
+                    </div>
+                    <textarea class="border rounded p-2 w-full" type="password" name="password"></textarea>
+                </div>
+
+                <div class="my-8 flex justify-between">
+                    <div class="pr-4">
+                        <button type="submit" class="inline-block rounded px-12 py-3 font-body text-2xl text-white bg-chocolate hover:bg-dark-chocolate hover:text-active-yellow">Submit!</button>
+                    </div>
+                    <p class="font-body md:max-w-md text-gray-500 md:text-2xl">By clicking Next, you will be brought to the a good event registration system to complete the registration process for this event.</p>
+                </div>
+
+            </form>
+        </div>
+
     </div>
-
-    <div class="flex justify-center">
-        <form class="max-w-sm md:max-w-4xl" action="/contact" method="post">
-
-            @csrf
-
-            @if ($errors->any())
-                <div class="bg-active-yellow rounded shadow-2xl p-4 mt-4">
-                    <h1 class="font-brand text-2xl">Hmm... Something went wrong.</h1>
-                    <ul class="list-disc pl-5">
-                        @foreach ($errors->all() as $error)
-                            <li class="font-body font-xl">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
-            <div class="my-8">
-                <div>
-                    <label class="font-body text-3xl pr-12" for="">What is your name?</label>
-                </div>
-                <input class="border rounded p-2 w-full" type="text" name="name">
-            </div>
-
-            <div class="my-8">
-                <div>
-                    <label class="font-body text-3xl pr-12" for="">What is your email?</label>
-                </div>
-                <input class="border rounded p-2 w-full" type="email" name="email">
-            </div>
-
-            <div class="my-8">
-                <div>
-                    <label class="font-body text-3xl pr-12" for="">What are ya inquiring about?</label>
-                </div>
-                <textarea class="border rounded p-2 w-full" type="password" name="password"></textarea>
-            </div>
-
-            <div class="my-8 flex justify-between">
-                <div class="pr-4">
-                    <button type="submit" class="inline-block rounded px-12 py-3 font-body text-2xl text-white bg-chocolate hover:bg-dark-chocolate hover:text-active-yellow">Submit!</button>
-                </div>
-                <p class="font-body md:max-w-md text-gray-500 md:text-2xl">By clicking Next, you will be brought to the a good event registration system to complete the registration process for this event.</p>
-            </div>
-
-        </form>
-    </div>
-
-</div>
-
-
-
-</body>
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,58 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>The Chocolate Milk Mile</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="https://use.typekit.net/suf2eff.css">
-</head>
+@extends('layouts.landing')
 
-<body>
+@section('title')
+    The Chocolate Milk Mile
+@endsection
 
-@include('layouts.nav')
+@section('content')
+    <div id="hero" class="h-full bg-center bg-no-repeat bg-cover bg-top bg-fixed pt-16" style="background-image: url('/img/hero.png')">
+        <h1 class="text-center py-2 text-5xl md:text-6xl text-white font-brand">August 1st, 2020</h1>
+        <h1 class="text-center py-0 text-4xl md:text-5xl text-white font-brand">at the</h1>
+        <h1 class="text-center py-2 text-5xl md:text-6xl text-white font-brand">Burnt Hills High School</h1>
 
-<div id="hero" class="h-full bg-center bg-no-repeat bg-cover bg-top bg-fixed pt-16" style="background-image: url('/img/hero.png')">
-    <h1 class="text-center py-2 text-5xl md:text-6xl text-white font-brand">August 1st, 2020</h1>
-    <h1 class="text-center py-0 text-4xl md:text-5xl text-white font-brand">at the</h1>
-    <h1 class="text-center py-2 text-5xl md:text-6xl text-white font-brand">Burnt Hills High School</h1>
-
-    <div class="flex justify-center">
-        <a href="/register" class="inline-block bg-active-yellow my-16 px-6 py-3 text-4xl text-chocolate font-brand hover:bg-white hover:text-chocolate rounded-lg shadow-2xl">Register today!</a>
-    </div>
-</div>
-
-<div class="md:flex md:justify-center" id="what">
-
-    <div class="md:ml-20 p-12 mt-16 mb-16">
-        <h2 class="font-brand text-chocolate text-6xl">What is this?!</h2>
-        <p class="font-body text-black text-5xl">In The Chocolate Milk Mile, all runners must drink 1 cup of chocolate milk upon completion of every lap, equaling a half gallon of chocolate milk in total!</p>
-        <p class="font-body text-gray-500 text-2xl">(simply put, it’s just a really great time)</p>
+        <div class="flex justify-center">
+            <a href="/register" class="inline-block bg-active-yellow my-16 px-6 py-3 text-4xl text-chocolate font-brand hover:bg-white hover:text-chocolate rounded-lg shadow-2xl">Register today!</a>
+        </div>
     </div>
 
-    <div class="">
-        <img class="hidden md:block" src="/img/milk.png" alt="A glass of thick, creamy, delicious chocolate milk.">
+    <div class="md:flex md:justify-center" id="what">
+
+        <div class="md:ml-20 p-12 mt-16 mb-16">
+            <h2 class="font-brand text-chocolate text-6xl">What is this?!</h2>
+            <p class="font-body text-black text-5xl">In The Chocolate Milk Mile, all runners must drink 1 cup of chocolate milk upon completion of every lap, equaling a half gallon of chocolate milk in total!</p>
+            <p class="font-body text-gray-500 text-2xl">(simply put, it’s just a really great time)</p>
+        </div>
+
+        <div class="">
+            <img class="hidden md:block" src="/img/milk.png" alt="A glass of thick, creamy, delicious chocolate milk.">
+        </div>
+
     </div>
 
-</div>
-
-<div class="flex justify-center bg-chocolate p-20 text-center bg-no-repeat bg-cover" style="background-image: url('/img/ripple.png')">
-    <div class="max-w-3xl">
-        <h1 class="font-body text-white lg:text-6xl md:text-5xl text-4xl">This race is being put on to celebrate the life of Noah Farrelly, who loved running.</h1>
-        <p class="font-body text-white lg:text-4xl md:text-3xl text-2xl">All of the money (that we don’t spend on chocolate milk) will be donated to his family.</p>
-    </div>
-</div>
-
-<div class="md:flex md:justify-center" id="prizes">
-
-    <div class="md:ml-20 p-12 mt-16 mb-16">
-        <img class="-ml-48" src="/img/splash.png" alt="">
+    <div class="flex justify-center bg-chocolate p-20 text-center bg-no-repeat bg-cover" style="background-image: url('/img/ripple.png')">
+        <div class="max-w-3xl">
+            <h1 class="font-body text-white lg:text-6xl md:text-5xl text-4xl">This race is being put on to celebrate the life of Noah Farrelly, who loved running.</h1>
+            <p class="font-body text-white lg:text-4xl md:text-3xl text-2xl">All of the money (that we don’t spend on chocolate milk) will be donated to his family.</p>
+        </div>
     </div>
 
-    <div>
-        <h2 class="font-brand text-chocolate text-6xl">Are there prizes?</h2>
-        <p class="font-body text-black text-5xl">Of course! The top 5 places in the race will receive a t-shirt and some sweet bragging rights.</p>
+    <div class="md:flex md:justify-center" id="prizes">
+
+        <div class="md:ml-20 p-12 mt-16 mb-16">
+            <img class="-ml-48" src="/img/splash.png" alt="">
+        </div>
+
+        <div>
+            <h2 class="font-brand text-chocolate text-6xl">Are there prizes?</h2>
+            <p class="font-body text-black text-5xl">Of course! The top 5 places in the race will receive a t-shirt and some sweet bragging rights.</p>
+        </div>
+
     </div>
 
-</div>
-
-<div id="footer" class="bg-chocolate flex text-center justify-center p-12 bg-cover bg-no-repeat" style="background-image: url('/img/ripple.png')">
-    <div>
-        <h1 class="text-white font-brand text-5xl">What're ya waiting for?!</h1>
-        <h1 class="text-white font-brand text-6xl">Register today!</h1>
-        <a href="/register" class="inline-block bg-active-yellow my-4 px-6 py-3 text-4xl text-chocolate font-brand hover:bg-white hover:text-chocolate rounded-lg shadow-2xl">Register!</a>
+    <div id="footer" class="bg-chocolate flex text-center justify-center p-12 bg-cover bg-no-repeat" style="background-image: url('/img/ripple.png')">
+        <div>
+            <h1 class="text-white font-brand text-5xl">What're ya waiting for?!</h1>
+            <a href="/register" class="inline-block bg-active-yellow my-4 px-6 py-3 text-4xl text-chocolate font-brand hover:bg-white hover:text-chocolate rounded-lg shadow-2xl">Register today!</a>
+        </div>
     </div>
-</div>
-
-
-
-</body>
-</html>
-<script>
-    import NavigationBar from "../js/components/NavigationBar";
-    export default {
-        components: {NavigationBar}
-    }
-</script>
+@endsection
