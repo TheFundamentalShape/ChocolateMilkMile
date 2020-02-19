4a2297d3e4503315a04b600e493a1cf62a5822dc
Nick Hunziker <hunzi@nicks-mbp-2.iona.edu>
2020-02-18 23:52:02 -0500

    added photos, fixed typos

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -10,7 +10,7 @@
 
         <div class="pb-10">
             <h1 class="text-center text-5xl md:text-6xl text-brown fat-font">Here's some photos!</h1>
-            <p class="text-center text-gray-600 text-2xl skippy-font">(All of these beauties were taken by <a class="underline" href ="https://www.josephkhale.com/">Joseph Hale.</a>)</p>
+            <p class="text-center text-gray-600 text-2xl skippy-font">(All of these beauties were taken by photographers <a class = "underline" href = "https://www.jaybendlinphoto.com/">Jay Blendin</a> and <a class="underline" href ="https://www.josephkhale.com/">Joseph Hale.</a>)</p>
         </div>
 
         <div class = "md:flex md:flex-wrap px-5">
@@ -18,10 +18,10 @@
                 <img class = "block h-auto w-full" src='/pics/clai.png'>
             </div>
             <div class = "mb-4 px-2 w-full md:w-1/3">
-                <img class = "block h-auto w-full" src='/pics/don.png'>
+                <img class = "block h-auto w-full" src='/pics/emily.png'>
             </div>
             <div class = "mb-4 px-2 w-full md:w-1/3">
-                <img class = "block h-auto w-full" src='/pics/emily.png'>
+                <img class = "block h-auto w-full" src='/pics/puke.png'>
             </div>
         </div>
         <div class = "md:flex md:flex-wrap px-5">
@@ -76,7 +76,7 @@
                 <img class = "block h-auto w-full" src='/pics/park.png'>
             </div>
             <div class = "mb-4 px-2 w-full md:w-1/3">
-                <img class = "block h-auto w-full" src='/pics/mary.png'>
+                <img class = "block h-auto w-full" src='/pics/don.png'>
             </div>
         </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
             <a href="/register" class="hover:underline text-brown hover:text-dark-brown"><h1 class="fat-font text-6xl">Register Now!</h1></a>
             <p class="skippy-font text-brown text-2xl">
                 All of the proceeds from this event are going to the Noah Farrelly Memorial Fund. You can donate by participating in the event, or purchasing a T-shirt.
-                You can purchase a T-shirt for $15 dollars, you can register for $7, or you can both for $20!
+                You can purchase a T-shirt for $15 dollars, you can register for $7, or you can do both for $20!
                 We'll also be accepting donations of shoes and running flats on the day of the event.
             </p>
         </div>
